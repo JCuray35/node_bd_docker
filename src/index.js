59bd9607be0c9b8ea1235ca3b46b5844f4753598
Junior Curay <juniorcurayhualpa35@gmail.com>
2026-09-03 16:37:22 -0500

    proyecto de base de node

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+// Inicializando una app de express
+const app = express()
+
+app.get("/", function(req, res){
+    return res.json({message: "Saludos desde mi App de Node (MODIFICADO) último"});
+});
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo en el puerto 3000 (ultima modificacion");
+})
