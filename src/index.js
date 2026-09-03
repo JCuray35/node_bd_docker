17f1fa2717531cd6a54f8ff88e48d4e817cc55c9
Junior Curay <juniorcurayhualpa35@gmail.com>
2026-09-03 17:43:31 -0500

    docker compose

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res){
-    return res.json({message: "Saludos desde mi App de Node (MODIFICADO) último"});
+    return res.json({message: "Saludos desde mi App de Node (MODIFICADO) último 2"});
 });
 
 app.listen(3000, function(){
-    console.log("Servidor corriendo en el puerto 3000 (ultima modificacion");
+    console.log("Servidor corriendo en el puerto 3000 (ultima modificacion 2)");
 })
