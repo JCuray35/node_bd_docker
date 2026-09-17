b9f9651672b46d458487da2214fbd6a1c829a8ef
Junior Curay <juniorcurayhualpa35@gmail.com>
2026-09-17 13:49:10 -0500

    Docker configuraciones extras en docker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require("express")
+require("dotenv").config();
+
 const User = require("./models/user")
 const Producto = require("./models/producto")
 
 // Inicializando una app de express
 const app = express()
 
+const port = process.env.PORT || 3000;
+
 app.get("/", function(req, res){
     return res.json({message: "Saludos desde mi App de Node (MODIFICADO) último 2"});
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Servidor corriendo en el puerto 3000 (ultima modificacion 2)");
 })
